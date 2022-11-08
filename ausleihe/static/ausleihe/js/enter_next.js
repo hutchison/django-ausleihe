3da0a399a812a32c7daf57bbe4eeaece7fa8d5a9
Martin Darmüntzel <martin@trivialanalog.de>
2022-11-08 22:27:03 +0100

    Ersetzt select2 durch selectize.

--- ausleihe/static/ausleihe/js/enter_next.js
+++ ausleihe/static/ausleihe/js/enter_next.js
@@ -1,8 +1,16 @@
 function enter_next(event) {
 	if (event.which == 13) {
 		event.preventDefault();
-		var $this = $(event.target);
-		var index = parseInt($this.attr("data-index"));
-		$('[data-index="' + (index + 1).toString() + '"]').focus();
+		let index = parseInt($(event.target).attr("data-index"));
+		var next_element = $('[data-index="' + (index + 1).toString() + '"]');
+
+		if (next_element.length == 1) {
+			var e = next_element[0];
+			if (e.classList.contains("selectized")) {
+				e.selectize.focus();
+			} else {
+				e.focus();
+			}
+		}
 	}
 }
