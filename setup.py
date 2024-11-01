12e1e4adef270551963a94695d5d9d5aa0a770ff
Martin Darmüntzel <martin@trivialanalog.de>
2024-11-01 16:45:32 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ausleihe',
-    version='0.3.7',
+    version='0.3.8',
     packages=find_packages(),
     include_package_data=True,
     description='Django-App für die Ausleihe von Medien.',
